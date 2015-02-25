f2b74adbd7692093ffff0fa32fd575b1aee4a7de
LlanezaAller <llan090@gmail.com>
2015-02-25 11:35:32 +0100

    Comprobación de fallo de nombre en archivo

--- Parser/src/Main.java
+++ Parser/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -10,15 +11,19 @@ import preguntas.Pregunta;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Inserte el Nombre del Fichero de Preguntas.");
 		Parser p = new Parser(s.nextLine());
+		
 		try {
 			HashMap<String,Pregunta> preguntasFinal = p.getPregunta();
 			for(Entry<String, Pregunta> entry : preguntasFinal.entrySet())
 				System.out.println(entry.toString());
+			
+		} catch (FileNotFoundException e){
+			System.out.println("No existe el archivo.");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		s.close();

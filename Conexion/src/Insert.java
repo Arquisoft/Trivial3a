2435bae6e948c90ccef8cd49271c28581aca2c4e
Marcosafm <marckitos1@gmail.com>
2015-02-28 12:54:44 +0100

    Actualizando el insert

--- Conexion/src/Insert.java
+++ Conexion/src/Insert.java
@@ -1,7 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.util.JSON;
+import com.mongodb.util.JSONParseException;
 
 
 
@@ -9,19 +18,44 @@ public class Insert {
 
 	private static DB db;
 	private static DBCollection collection;
+	private static FileInputStream archivo;
 	
 	public static void main(String[] args) {
 		db = Connection.DatabaseConnection();
-		insertQuestions();
+		collection = db.getCollection("Questions");
+		importJSon("");
+		insert();
 		getElements();
 
 	}
 	
-	public static void insertQuestions(){
-		collection = db.getCollection("T");
-		db.command("mongoimport --db TrivialDB --collection T --file Prueba.JSON");	
-		System.out.println("Hay "+collection.count()+" preguntas en esta colección");
-
+	public static void importJSon(String path){
+		try{
+			archivo = new FileInputStream("P.JSON");
+		}catch(FileNotFoundException e){
+			System.out.println("El archivo de preguntas no ha sido encontrado");
+		}
+	}
+	
+	public static void insert(){
+		BufferedReader br = new BufferedReader(new InputStreamReader(archivo));
+		String line;
+		int cont = 0;
+		try{
+		while((line = br.readLine()) != null){
+			try{
+			cont++;
+			DBObject obj = (DBObject) JSON.parse(line);
+			collection.insert(obj);
+			}catch(DuplicateKeyException e){
+				System.out.println("La pregunta de la línea "+cont+" ya se encuentra en la base de datos");
+			}catch(JSONParseException e1){
+				System.out.println("La línea "+cont+" se encuentra en un formato no válido");
+			}
+		}
+		}catch(IOException e){
+			System.out.println("Ha ocurrido un error al leer la línea "+cont);
+		}
 	}
 
 	/*

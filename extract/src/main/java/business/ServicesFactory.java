02f1b661db8b5ea9010a750861082a2b1a7c0821
LlanezaAller <llan090@gmail.com>
2015-04-06 18:30:54 +0200

    Comentarios

--- extract/src/main/java/business/ServicesFactory.java
+++ extract/src/main/java/business/ServicesFactory.java
@@ -5,11 +5,35 @@ import business.parser.ServiceParser;
 import business.preguntas.ServicePreguntas;
 import business.serializador.ServiceSerializador;
 
+/**
+ * Factoría de la Logica
+ * @author TeLoRompoTo
+ *
+ */
 public interface ServicesFactory {
 
+	/**
+	 * Crea la el servicio de los extractores
+	 * @return Clase extractor
+	 */
 	ServiceExtractor createServiceExtractor();
+	
+	/**
+	 * Crea el servicio del parser
+	 * @return Clase Parser
+	 */
 	ServiceParser createServiceParser();
+	
+	/**
+	 * Crea el servicio de las preguntas
+	 * @return Clase Pregunta
+	 */
 	ServicePreguntas createServicePreguntas();
+	
+	/**
+	 * Crea el servicio del Serializador
+	 * @return Clase Serializador
+	 */
 	ServiceSerializador createServiceSerializador();
 	
 }
--- extract/src/main/java/infraestructura/Factories.java
+++ extract/src/main/java/infraestructura/Factories.java
@@ -7,6 +7,12 @@ import business.SimpleServiceFactory;
 
 public class Factories {
 
+	/**
+	 * Factoria Business
+	 */
 	public static ServicesFactory services = new SimpleServiceFactory();
+	/**
+	 * Factoria Persitencia
+	 */
 	public static PersistenceFactory persistence = new SimplePersistenceFactory();
 }

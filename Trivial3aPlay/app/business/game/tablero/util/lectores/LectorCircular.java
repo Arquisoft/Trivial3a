5c38b13735dd0ae018816edf1d82f29248e0e70e
uo232562 <uo232562@uniovi.es>
2015-05-03 18:22:02 +0200

    Arreglado lector tableros

--- Trivial3aPlay/app/business/game/tablero/util/lectores/LectorCircular.java
+++ Trivial3aPlay/app/business/game/tablero/util/lectores/LectorCircular.java
@@ -3,7 +3,9 @@ package business.game.tablero.util.lectores;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 
+import play.Play;
 import business.game.tablero.nodos.impl.NodoQuesito;
 import business.game.tablero.util.ContenedorDeNodos;
 import business.game.tablero.util.SelectorDeColores;
@@ -12,7 +14,8 @@ public class LectorCircular implements LectorTipoTablero {
 
 	@Override
 	public ContenedorDeNodos cargarTablero(String fichero) throws IOException {
-		BufferedReader BR = new BufferedReader(new FileReader(fichero));
+		URL file = Play.application().classloader().getResource(fichero);
+		BufferedReader BR = new BufferedReader(new FileReader(file.getPath()));
 		ContenedorDeNodos contenedor = new ContenedorDeNodos();
 		try {
 			String linea;

6bfd217d7c97603a18ddf0a7d38a628ecd464d57
uo232562 <uo232562@uniovi.es>
2015-05-03 14:46:03 +0200

    Vistas compatibles con play y actualizadas las rutas

--- Trivial3aPlay/app/controllers/Application.java
+++ Trivial3aPlay/app/controllers/Application.java
@@ -8,7 +8,16 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
+    }
+    public static Result menu() {
+        return ok(menu.render());
+    }
+    public static Result createGame() {
+        return ok(selectBoard.render());
+    }
+    public static Result game() {
+        return ok(game.render());
     }
 
 }
---
+++ Trivial3aPlay/public/javascripts/logoAnimation.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    		$("#spin")
+    			.velocity({  rotateZ: "90deg" }, { loop: true, duration: 1000, reverse:false});
+});

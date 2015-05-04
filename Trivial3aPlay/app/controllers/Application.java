9aab28f440c5f6e3538cfc2c6e58aedf35c295d1
uo232562 <uo232562@uniovi.es>
2015-05-04 15:24:45 +0200

    .

--- Trivial3aPlay/app/controllers/Application.java
+++ Trivial3aPlay/app/controllers/Application.java
@@ -106,42 +106,6 @@ public class Application extends Controller {
 			juego = (JuegoEnTableroLineal) Cache.get(session("gameID"));
 		}
 
-		// PruebaJSON
-		//String jsonTab = JSON.serialize(juego);
-		//JuegoEnTableroLineal j = (JuegoEnTableroLineal) JSON.parse(jsonTab);
-
 		return ok(game.render(juego));
 	}
-	
-	
-	public static Result move(String direction){
-		JuegoEnTableroLineal juego  = (JuegoEnTableroLineal) Cache.get(session("gameID"));
-		juego.lanzarDado();
-		switch (direction) {
-		case "up":
-			break;
-		case "down":
-			break;
-		case "left":
-			juego.jugarIzquierda();
-			break;
-		case "right":
-			juego.jugarDerecha();
-			break;
-		}
-		
-		return ok(juego.getTextoPregunta());
-	}
-	
-//	public static Result javascriptRoutes() {
-//	    response().setContentType("text/javascript");
-//	    return ok(
-//	        Routes.javascriptRouter("jsRoutes",
-//	            controllers.routes.javascript.Projects.add(),
-//	            controllers.routes.javascript.Projects.delete(),
-//	            controllers.routes.javascript.Projects.rename(),
-//	            controllers.routes.javascript.Projects.addGroup()
-//	        )
-//	    );
-//	}
 }
---
+++ Trivial3aPlay/app/controllers/Game.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.cache.Cache;
+import play.data.*;
+import static play.data.Form.*;
+
+import java.util.*;
+
+import business.game.tablero.mecanica.impl.JuegoEnTableroLineal;
+import models.*;
+import views.html.*;
+
+public class Game extends Controller{
+	public static Result move(String direction){
+		JuegoEnTableroLineal juego  = (JuegoEnTableroLineal) Cache.get(session("gameID"));
+		juego.lanzarDado();
+		switch (direction) {
+		case "up":
+			break;
+		case "down":
+			break;
+		case "left":
+			juego.jugarIzquierda();
+			break;
+		case "right":
+			juego.jugarDerecha();
+			break;
+		}
+		
+		return ok(juego.getTextoPregunta());
+	}
+}

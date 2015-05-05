83d2bf9ea919ba4b6b3f28e61b0f15f39b840fd3
pablobravo <pablobravomediavilla@gmail.com>
2015-05-05 16:00:52 +0200

    Añadida una prueba de cucumber para probar que se añaden jugadores a la partida.

---
+++ Trivial3aPlay/app/test/java/es/uniovi/asw/steps/PartidaSteps.java
@@ -0,0 +1,38 @@
+package test.java.es.uniovi.asw.steps;
+
+import java.util.List;
+
+import business.game.login.Partida;
+import business.game.tablero.colores.Color;
+import business.game.tablero.colores.Verde;
+import modelo.usuario.Usuario;
+import es.uniovi.asw.game.UserDb;
+import cucumber.api.java.es.*;
+import cucumber.api.PendingException;
+import static org.assertj.core.api.Assertions.*;
+
+public class PartidaSteps {
+	 
+	private Usuario usuario ;
+	private Boolean loginValue;
+	private Partida partida;
+	
+	@Dada("^una partida de trivial$")
+	public void una_partida_de_trivial() throws Throwable {
+		partida = new Partida();
+	}
+	
+	@Cuando("^añado un usuario de nombre \"(.*?)\" y clave \"(.*?)\"$")
+	public void añado_un_usuario_de_nombre_y_clave(String nombre, String clave) throws Throwable {
+		Color color = new Verde();
+		usuario.setNombre(nombre);
+		usuario.setPassword(clave);
+		partida.addUsuario(color, usuario);
+	}
+	
+	@Entonces("^el numero de jugadores es (\\d+)$")
+	public void el_numero_de_jugadores_es(int n) throws Throwable {
+	    assertThat(partida.getPartida().size()).isEqualTo(n);  
+	}
+	
+}

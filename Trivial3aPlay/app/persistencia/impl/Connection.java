2f53ed6aa62886cc7b72f18f56aa746ad8126dc9
Liliana <lilianavillar@hotmail.es>
2015-05-05 20:28:54 +0200

    Base de datos en servidor

--- Trivial3aPlay/app/persistencia/impl/Connection.java
+++ Trivial3aPlay/app/persistencia/impl/Connection.java
@@ -1,11 +1,10 @@
 package persistencia.impl;
 
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 
 public class Connection {
 
-	private final static String DB = "TrivialDB";
+	private final static String DB = "trivialdb";
 	
 	/**
 	 * Método que conecta la aplicación con el servidor
@@ -15,8 +14,10 @@ public class Connection {
 	public static DB DatabaseConnection(){
 		MongoClient mongo;
 		try {
-			mongo = new MongoClient("localhost");
-			return mongo.getDB(DB);
+			MongoClientURI uri  = new MongoClientURI("mongodb://lili:lili@ds031842.mongolab.com:31842/trivialdb"); 
+	        mongo = new MongoClient(uri);
+			//mongo = new MongoClient("localhost");
+			return mongo.getDB(uri.getDatabase());
 		} catch (Exception e) {
 			System.out.println("No se ha podido realizar la conexión con la base de datos");
 			return null;
